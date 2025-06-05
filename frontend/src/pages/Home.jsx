2f8e714128cf3048d106522dcff4b20c1f736453
Tavish Chawla <tchawla827@gmail.com>
2025-06-05 11:15:21 +0530

    fixed compilation error in home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext'\
+import { useAuth } from '../context/AuthContext'
 import { fetchRecentBuckets } from '../api'
 
 
